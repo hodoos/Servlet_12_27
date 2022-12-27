5b6c03df06704590b6e05da6e47d5c70b363082b
hodoos <motoboxx@naver.com>
2022-12-27 20:15:17 +0900

    [이종호] - 인코딩 필터 추가 - 페이지 인코딩 메소드 제거

---
+++ src/main/java/com/hodoos/servlet/config/EncodingFilter.java
@@ -0,0 +1,26 @@
+package com.hodoos.servlet.config;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class EncodingFilter implements Filter {
+
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		request.setCharacterEncoding("utf-8");
+		response.setCharacterEncoding("utf-8");
+		
+		chain.doFilter(request, response);
+		// 이거 한번 하면 모든인코딩이 필요없다함
+	}
+	
+	
+}
